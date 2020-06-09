bbcc099eac5f8ef85eaa54cfbfe8e26a43dd5abb
lekanda <abernaolao@gmail.com>
2020-06-09 14:56:13 +0200

    Crear la barra lateral

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React, {Fragment} from 'react';
 
 // Importar el Layout
 import Header from './componentes/layout/Header';
+import Navegacion from './componentes/layout/Navegacion';
 
 function App() {
   return(
     <Fragment>
       <Header />
+
+      <div class="grid contenedor contenido-principal">
+          <Navegacion />
+
+          <main class="caja-contenido col-9">
+              {/* // TODO: Routing a los diferentes componentes */}
+          </main>
+      </div>
     </Fragment>
 
   )
