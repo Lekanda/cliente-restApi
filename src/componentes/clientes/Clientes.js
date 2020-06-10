e88e950c97d29ccbe82c6043e580ca64c028dd2f
lekanda <abernaolao@gmail.com>
2020-06-10 16:09:17 +0200

    Guadar datos en State y useState

--- src/componentes/clientes/Clientes.js
+++ src/componentes/clientes/Clientes.js
@@ -1,23 +1,51 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
 // Importar cliente Axios
 import clienteAxios from '../../config/axios';
 
 
 function Clientes () {
+    // Trabajar con el State
+    // clientes = State
+    // guardarClientes = Funcion para guardar el State
+    const [clientes, guardarClientes] = useState({});
+
+
     //Query a la API
     const consultarAPI = async() => {
-        console.log('Consultando...');
+        // console.log('Consultando...');
         const clientesConsulta = await clienteAxios.get('/clientes');
-        console.log(clientesConsulta);
+        // console.log(clientesConsulta.data.clientes);
+
+        // Colocar el resultado en el State
+        guardarClientes(clientesConsulta.data.clientes);
     }
 
     //  Use effect es similar a componentdidmount y willmount
     useEffect( () => {
         consultarAPI();
-    },[]);
+    }, [] );
+
+
+    function createArray(clientes) {
+        if (clientes && clientes.length > 0) {
+          return clientes.map(cliente => console.log(cliente)
+          );
+        }
+        return [];
+      }
+
+
 
     return (
-        <h2>Clientes</h2>
+        <Fragment>
+            <h2>Clientes</h2>
+            {/* {console.log(clientes)} */}
+            {/* {console.log(guardarClientes)} */}
+            
+            <ul className="listado-clientes">
+                {createArray(clientes)}
+            </ul>
+        </Fragment>
     )
 }
 
