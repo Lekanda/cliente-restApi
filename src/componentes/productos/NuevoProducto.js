8d406f18234316f68d2e6893b22d88e2be14696d
lekanda <abernaolao@gmail.com>
2020-10-24 15:29:46 +0200

    Crear nuevos productos, su imagen y subiendo a la DB con  REACT

--- src/componentes/productos/NuevoProducto.js
+++ src/componentes/productos/NuevoProducto.js
@@ -1,12 +1,10 @@
 import React, {useState, Fragment} from 'react';
+import Swal from 'sweetalert2';
+import clienteAxios from '../../config/axios';
+import { withRouter} from 'react-router-dom';
 
 
-// Guardar Archivo
-
-
-
-
-function NuevoProducto () {
+function NuevoProducto (props) {
 
     // producto = state ; guardarProducto = setState
     const [producto, guardarProducto] = useState({
@@ -16,6 +14,47 @@ function NuevoProducto () {
     // archivo(imagen)=state, guardarArchivo= setState
     const [archivo, guardarArchivo] = useState('');
 
+    // Almacena el nuevo Producto en la DB y sube la imagen a la vez
+    const agregarProducto = async e => {
+        e.preventDefault();
+
+        // Crear un formData para subir los datos
+        const formData = new FormData();
+        formData.append('nombre', producto.nombre);
+        formData.append('precio', producto.precio);
+        formData.append('imagen', archivo);
+
+        //Almacenarlo en la DB
+        try {
+            const res = await clienteAxios.post('/productos', formData, {
+                headers: {
+                    'Content-Type' : 'multipart/form-data'
+                }
+            });
+            // console.log(res);
+
+            // Alerta para producto creado
+            if(res.status === 200) {
+                Swal.fire(
+                    'Agregado Correctamente',
+                    res.data.mensaje,
+                    'success'
+                )
+            }
+
+            // Redireccionar
+            props.history.push('/productos');
+
+        } catch (error) {
+            console.log(error);
+            // Lanza la alerta
+            Swal.fire({
+                type:'error',
+                title:'Hubo un error',
+                text:'Vuelva a intentarlo'
+            })
+        }
+    }
     // Leer los datos del formulario
     const leerInformacionProducto = e => {
         // console.log([e.target.name]);
@@ -26,22 +65,22 @@ function NuevoProducto () {
             [e.target.name] : e.target.value
             
        })
-        // console.log(producto);
+       // console.log(producto);
     }
 
     // Colaca la imagen en el state
     const leerArchivo = e => {
-        
         // console.log(e.target.files);
         guardarArchivo( e.target.files[0]);
     }
-
 
     return (
         <Fragment>
             <h2>Nuevo Producto</h2>
 
-            <form>
+            <form
+                onSubmit={agregarProducto}
+            >
                 <legend>Llena todos los campos</legend>
 
                 <div className="campo">
@@ -83,4 +122,4 @@ function NuevoProducto () {
     )
 }
 
-export default NuevoProducto;
+export default withRouter(NuevoProducto);
