70d64be0814ebe339572c2b6290154780dadf1b0
lekanda <abernaolao@gmail.com>
2020-10-25 14:30:08 +0100

    Consultar API y obtener producto

--- src/componentes/pedidos/NuevoPedido.js
+++ src/componentes/pedidos/NuevoPedido.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import clienteAxios from '../../config/axios';
 import FormBuscarProducto from './FormBuscarProducto';
+import Swal from 'sweetalert2';
 
 
 
@@ -10,6 +11,8 @@ function NuevoPedido(props) {
 
     // State
     const [cliente, guardarCliente] = useState({});
+    const [busqueda, guardarBusqueda] = useState('');
+
 
 
 
@@ -29,19 +32,36 @@ function NuevoPedido(props) {
     }, []);
 
 
-    const buscarProducto = () => {
+    const buscarProducto = async e => {
+        e.preventDefault();
 
+        // Obtener los productos de la busqueda
+        const resultadoBusqueda = await clienteAxios.post(`/productos/busqueda/${busqueda}`);
+
+        // Sí no hay resultado en la busqueda una alerta, si no, agregarlo al State
+        if(resultadoBusqueda.data[0]) {
+
+        }else {
+            // No hay resultados
+            Swal.fire({
+                type: 'error',
+                title: 'No hay resultados',
+                text:'No hay resultados'
+            })
+        }
+
+        console.log(resultadoBusqueda);
     }
-
-    const leerDatosBusqueda = () => {
-
+    // Almacena una busqueda en el State
+    const leerDatosBusqueda = e => {
+        guardarBusqueda(e.target.value);
     }
 
     return (
         <Fragment>
             <h2>Nuevo Pedido</h2>
 
-                <div class="ficha-cliente">
+                <div className="ficha-cliente">
                     <h3>Datos de Cliente</h3>
                     <p>Nombre: {cliente.nombre} {cliente.apellido}</p>
                     <p>Tlf: {cliente.telefono}</p>
@@ -52,31 +72,31 @@ function NuevoPedido(props) {
                     leerDatosBusqueda={leerDatosBusqueda}
                 />
 
-                <ul class="resumen">
+                <ul className="resumen">
                     <li>
-                        <div class="texto-producto">
-                            <p class="nombre">Macbook Pro</p>
-                            <p class="precio">$250</p>
+                        <div className="texto-producto">
+                            <p className="nombre">Macbook Pro</p>
+                            <p className="precio">$250</p>
                         </div>
-                        <div class="acciones">
-                            <div class="contenedor-cantidad">
-                                <i class="fas fa-minus"></i>
+                        <div className="acciones">
+                            <div className="contenedor-cantidad">
+                                <i className="fas fa-minus"></i>
                                 <input type="text"name="cantidad" />
-                                <i class="fas fa-plus"></i>
+                                <i className="fas fa-plus"></i>
                             </div>
-                            <button type="button" class="btnbtn-rojo">
-                                <i class="fasfa-minus-circle"></i>
+                            <button type="button" className="btnbtn-rojo">
+                                <i className="fasfa-minus-circle"></i>
                                     Eliminar Producto
                             </button>
                         </div>
                     </li>
                 </ul>
-                    <div class="campo">
+                    <div className="campo">
                         <label>Total:</label>
                         <input type="number" name="precio" placeholder="Precio" readonly="readonly" />
                     </div>
-                    <div class="enviar">
-                        <input type="submit" class="btn btn-azul" value="Agregar Pedido"/>
+                    <div className="enviar">
+                        <input type="submit" className="btn btn-azul" value="Agregar Pedido"/>
                     </div>
                 
         </Fragment>
