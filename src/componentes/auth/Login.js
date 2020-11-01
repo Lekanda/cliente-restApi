df9dde57abf148597700af8d6300a246c2aed1d0
lekanda <abernaolao@gmail.com>
2020-11-01 16:39:21 +0100

    Mostrando el mensaje de error para CORS

--- src/componentes/auth/Login.js
+++ src/componentes/auth/Login.js
@@ -49,11 +49,20 @@ function Login (props) {
             
 
         } catch (error) {
-            Swal.fire({
-                type:'error',
-                title: 'Hubo un error',
-                text: error.response.data.mensaje
-            })
+            if(error.response) {
+                Swal.fire({
+                    type:'error',
+                    title: 'Hubo un error',
+                    text: error.response.data.mensaje
+                })
+            } else {
+                Swal.fire({
+                    type:'error',
+                    title: 'Hubo un error',
+                    text: 'Hubo un error'
+                })
+            }
+            
         }
     }
 
